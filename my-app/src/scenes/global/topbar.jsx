0d636d356af4f113e5a95b5448d7ecd86486657f
Navjit Singh <navjitsingh250@gmail.com>
2024-02-01 14:55:49 -0500

    Edited Topbar, added icon buttons and added search bar

--- my-app/src/scenes/global/topbar.jsx
+++ my-app/src/scenes/global/topbar.jsx
@@ -4,11 +4,11 @@ import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlinedIcon";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlinedIcon";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlinedIcon";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlinedIcon";
-import SearchIcon from "@mui/icons-material/SearchIcon";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 
 const Topbar = () => {
@@ -17,8 +17,35 @@ const Topbar = () => {
     const colorMode = useContext(ColorModeContext);
 
     return (<Box display="flex" justifyContent="space-between" p={2}>
+        {/*Search Bar*/}
+        <Box display= "flex" backgroundColor={colors.primary[400]} 
+        borderRadius = "3px"
+        >
+            <InputBase sx = {{ ml: 2, flex: 1}} placeholder = "Search"/>
+            <IconButton type ="button" sx ={{ p: 1 }}>
+                <SearchIcon />
+            </IconButton>
+        </Box>
 
-        <IconButton sx={{ display: "flex"}}></IconButton>
+        {/*ICONS*/}
+        <Box display="flex">
+            <IconButton onClick={colorMode.toggleColormode}>
+                {theme.palette.mode === 'dark' ? (
+                    <DarkModeOutlinedIcon />
+                )  : (
+                    <LightModeOutlinedIcon />
+                )}
+            </IconButton>
+            <IconButton>
+                <NotificationsOutlinedIcon />
+            </IconButton>
+            <IconButton>
+                <SettingsOutlinedIcon />
+            </IconButton>
+            <IconButton>
+                <PersonOutlinedIcon />
+            </IconButton>
+        </Box>
     </Box>);
 };
 
